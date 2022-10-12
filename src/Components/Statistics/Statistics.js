3f8814cbead4536ae29e2b7d9dd28a35ccdbe843
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-10-12 17:24:49 +0600

    added line chart in app

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = [
+        {
+            name: "React",
+            marks: 6,
+            quiz: 8,
+            amt: 240
+        },
+        {
+            name: "JavaScript",
+            marks: 8,
+            quiz: 9,
+            amt: 240
+        },
+        {
+            name: "CSS",
+            marks: 4,
+            quiz: 8,
+            amt: 240
+        },
+        {
+            name: "Git",
+            marks: 9,
+            quiz: 11,
+            amt: 240
+        }
+    ];
     return (
         <div>
-            <h3>THis is satistics</h3>
+            <LineChart width={500} height={400} data={data} margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}>
+
+                <Line type="monotone" dataKey="quiz" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="marks" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+
+            </LineChart>
+
+
         </div>
     );
 };
